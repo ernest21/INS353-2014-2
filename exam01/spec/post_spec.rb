dff1d27f32843e3c0fb26bb72906e49a5a30d5ef
Ernesto Ricart <ernest2193@gmail.com>
2014-06-05 10:36:40 -0400

    Fix post_spec

--- exam01/spec/post_spec.rb
+++ exam01/spec/post_spec.rb
@@ -2,10 +2,8 @@ require 'spec_helper'
 
 describe Post do
   before :all do
-    entry = Post.new( User.new ("ernest"),"Tittle",  \
-    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",\
-      "2014-6-5"), ["lorem", "greek", "test"]
+    user = User.new ("ernest")
+    entry = Post.new( user,"Tittle", "Test Test Test","2014-6-5",["test","entry"])
     File.open "post.yml", 'w' do |f|
       f.write YAML::dump entry
     end
