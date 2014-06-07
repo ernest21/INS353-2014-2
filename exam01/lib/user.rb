b6f220786dfe93d03977d3287007cb3cab5e5c28
Ernesto Ricart <ernest2193@gmail.com>
2014-06-07 12:22:40 -0400

    really finish the exam

--- exam01/lib/user.rb
+++ exam01/lib/user.rb
@@ -1,6 +1,6 @@
 require_relative 'post.rb'
 class User
-  attr_accessor :user_name,:posts
+  attr_accessor :user_name
   def initialize (user_name)
     @user_name = user_name
     @posts = []
@@ -8,18 +8,24 @@ class User
 
   def add_post date, text, title
     post = Post.new self, title,text,date
-    posts << post
+    @posts << post
+  end
+
+  def posts
+    @posts.sort_by(&:date).reverse
   end
 
   def list
-    posts.each{|post| post.display_entry}
+    @posts.each{|post| post.display_entry}
   end
+
   def find_post_by_title title
-    posts.select{|post| post.title == title }.first
+    @posts.select{|post| post.title == title }.first
   end
+
   def tag_cloud
     tags=[]
-    posts.each{|post| post.tags.each{|tag| tags << tag}}
+    @posts.each{|post| post.tags.each{|tag| tags << tag}}
     t = tags.inject(Hash.new(0)) {|key,i| key[i] += 1; key }
     t.sort_by{|k,v| v}.reverse[0,5].each {|tag,count| puts "#{tag}: #{count}" }
   end
